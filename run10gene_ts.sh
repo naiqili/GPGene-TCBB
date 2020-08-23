6a21b16a12bce48b6529b95009031cef0676a101
Naiqi Li <lnq18@mails.tsinghua.edu.cn>
2020-08-23 11:09:49 +0800

    submission for review.

---
+++ run10gene_ts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for sizem in 40
+do
+    for cc in 10 50 100
+    do
+        echo "sizem $sizem"
+        infile=/home/linaiqi/Lab/data/gene/ts10gene201.txt
+        outfile=/home/linaiqi/Lab/data/gene/res/mf10gene/gpgene\_10nw\_cc$cc\_m$sizem
+        echo $outfile
+        python main_ts.py --infile $infile --outfile $outfile --sizem $sizem --cc $cc --gene 10
+    done
+done
